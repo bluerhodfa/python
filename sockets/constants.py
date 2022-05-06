237a7dc84f089e4369dbbaa305039d48f4cbbb76
theoboldalex <theoboldalex@gmail.com>
2022-05-06 23:37:23 +0100

    Simple client server app that echos a message

---
+++ sockets/constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+HOST = '127.0.0.1'
+PORT = 65432
+
---
+++ sockets/socket_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from constants import HOST, PORT
+import socket
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f'Connected by {addr}')
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+if __name__ == '__main__':
+    main()
