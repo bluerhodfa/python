cdf5b8dbb21db54024e24d05806317e77bb8e16c
theoboldalex <theoboldalex@gmail.com>
2022-05-06 21:06:37 +0100

    Use cli args to get domains into script
    doesn't work for all domains. Currently working with reddit.com
    but not google.com.

--- pydig.py
+++ pydig.py
@@ -1,16 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-from __future__ import print_function
-
+import re
 import socket
 import dns.resolver
+from sys import argv
 
-# Basic query
-for rdata in dns.resolver.query('www.bluerhodfa.com', 'CNAME'):
-    print(rdata.target)
+def main():
+    domains = argv[1:]
 
-# Set the DNS Server
-resolver = dns.resolver.Resolver()
-resolver.nameservers = [socket.gethostbyname('ns1.alextheobold.com')]
-for rdata in resolver.query('www.fatladtofitdad.com', 'CNAME'):
-    print(rdata.target)
+    for domain in domains:
+        print(f'-- DOMAIN: {domain}')
+        ip = dns.resolver.resolve(domain, 'A')
+        print(f'IP: {ip[0].to_text()}')
+
+        for rdata in dns.resolver.resolve(domain, 'NS'):
+            print(f'NS: {rdata.target.to_text()}')
+
+        fqn = domain if re.search('^www.', domain) else f'www.{domain}'
+        for rdata in dns.resolver.resolve(fqn, 'CNAME'):
+            print(f'CNAME: {rdata.target}')
+
+if __name__ == '__main__':
+    main()
